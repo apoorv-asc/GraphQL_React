c86c6d746ced5e74787832d57d5279bc19dac224
apoorv-asc <apoorvsingh.chauhan2019@vitstudent.ac.in>
2022-05-22 03:01:57 +0530

    Client Side

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -15,15 +15,16 @@ class BookList extends Component {
     displayBooks(){
         var data = this.props.data;
         if(data.loading)
-            return (<div>Loading books...</div>)
+        return (<div>Loading books...</div>)
         else
             return data.books.map(book =>{
                 return(
                     <li>{book.name}</li>
-                )
-            })
-    }
+                    )
+                })
+            }
     render(){
+        console.log(this.props);
         return(
             <div>
                 <ul id="book-list">
