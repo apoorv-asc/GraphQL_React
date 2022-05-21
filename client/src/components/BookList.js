576daf0916dd5afd939c5628e599f1f13c1a8f78
apoorv-asc <apoorvsingh.chauhan2019@vitstudent.ac.in>
2022-05-22 02:54:44 +0530

    Client Side

---
+++ client/src/components/BookList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`;
+
+class BookList extends Component {
+    displayBooks(){
+        var data = this.props.data;
+        if(data.loading)
+            return (<div>Loading books...</div>)
+        else
+            return data.books.map(book =>{
+                return(
+                    <li>{book.name}</li>
+                )
+            })
+    }
+    render(){
+        return(
+            <div>
+                <ul id="book-list">
+                    { this.displayBooks() }
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default graphql(getBooksQuery)(BookList);
